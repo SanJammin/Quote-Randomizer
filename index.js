b03a44b0f0902f0079087f272d0d4a017225f7e9
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-04-10 17:28:10 +0100

    Initial set up

---
+++ index.js
@@ -0,0 +1,35 @@
+const quotes = [
+    {
+      text: "The best way to get started is to quit talking and begin doing.",
+      author: "Walt Disney"
+    },
+    {
+      text: "You don’t have to be great to start, but you have to start to be great.",
+      author: "Zig Ziglar"
+    },
+    {
+      text: "The most courageous act is still to think for yourself. Aloud.",
+      author: "Coco Chanel"
+    },
+    {
+      text: "If you’re going through hell, keep going.",
+      author: "Winston Churchill"
+    },
+    {
+      text: "Creativity is intelligence having fun.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "Strive not to be a success, but rather to be of value.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "Done is better than perfect.",
+      author: "Sheryl Sandberg"
+    },
+    {
+      text: "Build things you wish existed.",
+      author: "Sahil Lavingia"
+    }
+  ];
+  
